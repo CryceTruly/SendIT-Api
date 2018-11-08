eb24d77a6a3f95c402c60a8a056632cca3e0459a
crycetruly <crycetruly@gmail.com>
2018-11-08 20:09:33 +0300

    added the cancel parcel request route

--- app/endpoints.py
+++ app/endpoints.py
@@ -7,21 +7,19 @@ app = Flask(__name__)
 # GET parcels
 @app.route('/parcels')
 def get_parcels():
-    return jsonify({'parcels': parcels}),200
+    return jsonify({'parcels': parcels}), 200
 
 
 # GET parcels/id
 @app.route('/parcels/<int:id>')
 def get_a_parcel(id):
     parcel = {}
-    if not  parcel in parcels:
-        return jsonify({"message":"Parcel request not found ".format(parcel)}),404
     for parcel in parcels:
         if parcel['id'] == id:
             parcel = {
-                'id': 1,
+                'id': id,
                 'pickup_address': parcel['pickup_address'],
-                'destination_adress': parcel['destination_adress'],
+                'destination_address': parcel['destination_address'],
                 'comment_description': parcel['comment_description'],
                 'status': parcel['status'],
                 'current_location': parcel['current_location'],
@@ -32,7 +30,7 @@ def get_a_parcel(id):
                 'recipient_email': parcel['recipient_email']
 
             }
-            return jsonify(parcel),200
+            return jsonify(parcel), 200
 
     return jsonify(parcel)
 
@@ -43,7 +41,7 @@ def add_parcel():
     request_data = request.get_json()
     if (is_valid_request(request_data)):
         parcel = {
-            'id': 1,
+            'id': len(parcels)+1,
             'pickup_address': request_data['pickup_address'],
             'destination_address': request_data['destination_address'],
             'comment_description': request_data['comment_description'],
@@ -58,7 +56,7 @@ def add_parcel():
         parcels.append(parcel)
         response = Response("", 201, mimetype="application/json")
         response.headers['Location'] = "parcels/" + str(request_data['id'])
-        return jsonify({"msg":"parcel delivery requests created"},parcel)
+        return jsonify({"msg": "parcel delivery requests created"}, parcel)
     else:
         bad_object = {
             "error": "Invalid Parcel delivery order object"
@@ -67,24 +65,30 @@ def add_parcel():
         return response
 
 
-# PATCH /parcels/id
-@app.route('/parcels/<int:id>', methods=['PATCH'])
-def update_parcel(id):
-    request_data = request.get_json()
-    updated_parcel = {}
-
-    if ("pickup_address" in request_data):
-        update_parcel["pickup_adress"] = request_data["pickup_adress"]
-
-    if ("destination_address" in request_data):
-        update_parcel["destination_adress"] = request_data["destination_adress"]
-
+# PUT PUT /parcels/<parcelId>/cancel
+# CANCELS A SPECIFIC RESOURCE BASING ON THE PROVIDED IDENTIFIER
+@app.route('/parcels/<int:id>/cancel', methods=['PUT'])
+def cancel_parcel_request(id):
+    cancelled_parcel = {}
     for parcel in parcels:
-        if parcel["id"] == id:
-            parcel.update(update_parcel)
-    response = Response("", status="204")
-    response.headers['Location'] = "/parcel" + str(id)
-    return response
+        if parcel['id'] == id:
+            cancelled_parcel = {
+                'id': parcel['id'],
+                'pickup_address': parcel['pickup_address'],
+                'destination_address': parcel['destination_address'],
+                'comment_description': parcel['comment_description'],
+                'status': "cancelled",
+                'current_location': parcel['current_location'],
+                'created': parcel['created'],
+                'user_id': parcel['user_id'],
+                'recipient_address': parcel['recipient_address'],
+                'recipient_phone': parcel['recipient_phone'],
+                'recipient_email': parcel['recipient_email']
+
+            }
+            parcel.update(cancelled_parcel)
+
+    return jsonify({"msg": "parcel request cancelled"}, cancelled_parcel)
 
 
 def is_valid_request(newparcel):
@@ -98,7 +102,7 @@ def is_valid_request(newparcel):
 parcels = [
     {
         'id': 1,
-        'pickup_adress': 'Kampala Kikoni Makerere 13',
+        'pickup_address': 'Kampala Kikoni Makerere 13',
         'destination_address': 'Mabarara Kikoni Home 13',
         'comment_description': 'My parcels contain a laptop,please deliver',
         'status': 'In Transit',
@@ -113,7 +117,7 @@ parcels = [
     {
         'id': 2,
         'pickup_address': 'Kampala Kikoni Makerere 13',
-        'destination_adress': 'Mabarara Kikoni Home 13',
+        'destination_address': 'Mabarara Kikoni Home 13',
         'comment_description': 'My parcels contain a laptop,please deliver',
         'status': 'In Transit',
         'current_location': 'Mabarara Kikoni Home 13',
