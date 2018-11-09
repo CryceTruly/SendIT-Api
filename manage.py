7b7d4da2481be54a7a67dbcfea40338ef5ca7e07
crycetruly <crycetruly@gmail.com>
2018-11-09 15:58:54 +0300

    added command for coverage to the manage file

--- manage.py
+++ manage.py
@@ -1,32 +1,40 @@
-import os
 import unittest
+import os
+import coverage
+
 from flask_script import Manager
 from app.endpoints import app
 
-# Initializing the manager
 manager = Manager(app)
 
 
-# Test coverage configuration
-COV = coverage.coverage(
-    branch=True,
-    include='app/*',
-    omit=[
-        'app/__init__.py'
-    ]
-)
-COV.start()
 
-
-# Add test command
 @manager.command
 def test():
-    """
-    Run tests without coverage
-    :return:
-    """
-    tests = unittest.TestLoader().discover('tests', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
+    """Runs the unit tests without coverage."""
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    cov = coverage.coverage(
+        branch=True,
+        include='app/*'
+    )
+    cov.start()
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    cov.stop()
+    cov.save()
+    print('Coverage Summary:')
+    cov.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir, 'coverage')
+    cov.html_report(directory=covdir)
+    cov.erase()
+
+
+if __name__ == '__main__':
+    manager.run()
