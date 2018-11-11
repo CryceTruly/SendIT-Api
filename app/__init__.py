cd55ddb4d46181f7b498bb4d6007ea3665b78427
crycetruly <crycetruly@gmail.com>
2018-11-11 07:47:56 +0300

    registered theusers and  parcels blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask 
-from app.endpoints import ap
-from app import users, endpoints
+from app.parcels import ap
+from app import users, parcels
 from app.users import user_print
 app = Flask(__name__)
 app.register_blueprint(ap)
