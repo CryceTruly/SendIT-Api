59099da55763b3c7a50f5d60c6d15ac2c63a0394
crycetruly <crycetruly@gmail.com>
2018-11-11 07:46:41 +0300

    added the users blueprint

--- app/users.py
+++ app/users.py
@@ -1,48 +1,23 @@
 from flask import Flask, jsonify, request, Response, json, Blueprint
-from app.endpoints import parcels
+from app.parcels import parcels
 import datetime
-from validate_email_address import validate_email
 import re
 
 user_print = Blueprint('user_print', __name__)
 
+
 @user_print.route('/api/v1/users', methods=['GET'])
 def getall_users():
-    return jsonify({"users": users}), 200
+    ''''
+    returns a list of all users
+    '''
+    if len(users) == 0:
+        return jsonify({"msg": "No users yet", "count": len(users)}), 200
+
+    return jsonify({"users": users, "count": len(users)}), 200
 
 
-users = [
-    {
-        "user_id": 1,
-        "fullname": "Cryce Truly",
-        "username": "crycetruly",
-        "phone_number": "+256756698765",
-        "email": "shelan@gmail.com",
-        "password": "14testwerek "
-
-    }
-    ,
-    {
-        "user_id": 2,
-        "fullname": "Shearan King",
-        "username": "sherah",
-        "phone_number": "+256756698765",
-        "email": "crycetruly@gmail.com",
-        "password": "sherah ",
-        "joined": datetime.datetime.now()
-
-    },
-    {
-        "user_id": 3,
-        "fullname": "Uncle Kiiza ",
-        "username": "kw@gmail",
-        "phone_number": "+256756698765",
-        "email": "kwagala@gmail.com",
-        "password": "14testwerek ",
-        "joined": datetime.datetime.now()
-
-    }
-]
+users = []
 
 
 @user_print.route('/api/v1/users', methods=['POST'])
@@ -52,45 +27,49 @@ def create_user():
     if not is_valid_user_request(request_data):
         return jsonify({"success": False, "msg": "Bad request"}), 400
 
-    if not validate_email(request_data['email']):
+    if not is_valid(request_data['email']):
         return jsonify({"success": False, "msg": "Email is badly formatted"}), 401
-            
+    newuser = {
+        "user_id": len(users) + 1,
+        "fullname": request_data['fullname'],
+        "username": request_data['username'],
+        "phone_number": request_data['phone_number'],
+        "email": request_data['email'],
+        "password": request_data['password'],
+        "joined": datetime.datetime.now()
 
-    
+    }
+    if len(users) == 0:
+        users.append(newuser)
+        return jsonify({"success": True, "user_id": newuser.get('user_id')}), 201
+
     for user in users:
         if user['email'] == request_data['email']:
             return jsonify({"success": False, "msg": "Email is already taken"}), 401
         if user['username'] == request_data['username']:
             return jsonify({"success": False, "msg": "Username is already taken"}), 401
-
-        newuser = {
-            "user_id": len(users) + 1,
-            "fullname": request_data['fullname'],
-            "username": request_data['username'],
-            "phone_number": request_data['phone_number'],
-            "email": request_data['email'],
-            "password": request_data['password'],
-            "joined": datetime.datetime.now()
-
-        }
-
-        users.append(newuser)
-        return jsonify({"success": True, "user_id": newuser.get('user_id')}), 201
-    
+    users.append(newuser)
+    return jsonify({"success": True, "user_id": newuser.get('user_id')}), 201
 
 
 @user_print.route('/api/v1/users/<int:id>/parcels')
 def get_user_parcels(id):
+    """
+    returns parcel requests created by a user given the users id
+    """
     user_parcels = []
     for parcel in parcels:
         if parcel['user_id'] == id:
             user_parcels.append(parcel)
 
-    
     return jsonify({"user_parcel_orders": user_parcels, "count": len(user_parcels)}), 200
 
 
 def is_valid_user_request(newuser):
+    """
+    helper to check required fields
+    """
+
     if "fullname" in newuser and "fullname" in newuser and "phone_number" in newuser and \
             "email" in newuser and "password" in newuser:
         return True
@@ -98,3 +77,9 @@ def is_valid_user_request(newuser):
         return False
 
 
+def is_valid(email):
+    """helper for chcking valid emails"""
+
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return False
+    return True
