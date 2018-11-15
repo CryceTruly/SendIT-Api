9844b6290a91b5ca4458ae351348ec86bf19d58d
crycetruly <crycetruly@gmail.com>
2018-11-15 06:40:17 +0300

    updated config

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
 from app import app
-
-#------------------INITIAL CONFIGURATIONS-----------------------------
-class BaseConfig(object):
-    DEBUG=False
-
-#------------------DEVELOPMENT ENVIRONMENT CONFIGURATIONS-----------------------------
-class DevConfig(BaseConfig):
+'''
+environment config
+'''
+class Config():
     DEBUG=True
     TESTING=True
     app.config['MAIL_SERVER']='smtp.gmail.com'
@@ -15,13 +12,4 @@ class DevConfig(BaseConfig):
     app.config['MAIL_PASSWORD'] = 'Xvq6thC++'
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = True
-#------------------PRODUCTION ENVIRONMENT CONFIGURATIONS-----------------------------
-class ProdConfig(BaseConfig):
-    DEBUG=False
-
-#------------------PRODUCTION ENVIRONMENT CONFIGURATIONS-----------------------------
-class TestConfig(BaseConfig):
-    DEBUG=True
-
-
 
