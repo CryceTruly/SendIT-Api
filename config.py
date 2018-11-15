134d8679d610787f86bf26413fadb33035e609bb
crycetruly <crycetruly@gmail.com>
2018-11-15 19:10:46 +0300

    updated config

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class DevConfig(BaseConfig):
     TESTING=True
     app.config['MAIL_SERVER']='smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = 'crycetruly@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'Xvq6thC++'
+    app.config['MAIL_USERNAME'] = os.environ.get('trulysEmail')
+    app.config['MAIL_PASSWORD'] = os.environ.get('trulysPassword')
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = True
 #------------------PRODUCTION ENVIRONMENT CONFIGURATIONS-----------------------------
