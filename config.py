e521fbdeaa5faa81d9f4ce88a5d18f3673e9788d
crycetruly <crycetruly@gmail.com>
2018-11-15 06:17:37 +0300

    added configuration for flask mail

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from app import app
 
 #------------------INITIAL CONFIGURATIONS-----------------------------
 class BaseConfig(object):
@@ -8,6 +9,12 @@ class BaseConfig(object):
 class DevConfig(BaseConfig):
     DEBUG=True
     TESTING=True
+    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = 'crycetruly@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'Xvq6thC++'
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = True
 #------------------PRODUCTION ENVIRONMENT CONFIGURATIONS-----------------------------
 class ProdConfig(BaseConfig):
     DEBUG=False
